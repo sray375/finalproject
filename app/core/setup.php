65aa55bd31542b3c92d16a977a0b543198d5b45d
Sandhya Ray <sandhyaray@sandhyas-air.myfiosgateway.com>
2025-05-15 00:31:02 -0400

    connect to raily MySQL via public network

--- app/core/setup.php
+++ app/core/setup.php
@@ -10,8 +10,8 @@ require_once __DIR__ . '/../models/User.php';
 
 
 
-define('DBNAME', 'your_database_name');
-define('DBHOST', 'your_database_host');  // usually 'localhost' or '127.0.0.1'
-define('DBUSER', 'your_database_user');
-define('DBPASS', 'your_database_password');
+define('DBHOST', 'crossover.proxy.rlwy.net');
+define('DBNAME', 'railway');
+define('DBUSER', 'root');
+define('DBPASS', 'your_real_password'); // paste from Railway
 
