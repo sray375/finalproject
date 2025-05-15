16792e8e07a4a2970e86e3b1afbb84924a29c49a
Sandhya Ray <sandhyaray@sandhyas-air.myfiosgateway.com>
2025-05-14 23:12:35 -0400

    Add package.json

--- app/controllers/AlbumController.php
+++ app/controllers/AlbumController.php
@@ -14,3 +14,5 @@ class AlbumController extends Controller {
 
     }
 }
+
+
--- public/assets/views/albums/honeymoon.php
+++ public/assets/views/albums/honeymoon.php
@@ -14,7 +14,7 @@
     <img src="/assets/images/honeymoon.jpg" alt="Honeymoon Album Cover" width="300" />
 
     <h2>Honeymoon</h2>
-    <p><em>"We both know the history of violence that surrounds you, But I'm not scared/There's nothing to lose now that I've found you"</em></p>
+    <p><em>"We both know the history of violence that surrounds you, But I'm not scared / There's nothing to lose now that I've found you"</em></p>
     <p>lana feels safe in a dangerous man. any hurt he inflicts on her won't matter. she has everything because she has him. </p>
 
     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4X5zaUdlRhvBWYnyQIKmH8" width="300" height="80" frameborder="0" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
